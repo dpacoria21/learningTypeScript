bd2697812a01f07e6813d30ab48fccc9cc259fb3
Diego Pacori <dpacoria@unsa.edu.pe>
2023-04-16 18:20:23 -0500

    Se escribio el primer codigo en TypeScript y para poder mostralo en el navegador, tuve que transpilarlo a JS y recien aparece

---
+++ bases/app.js
@@ -0,0 +1,6 @@
+var msg = 'Hola mundo';
+var num = 123;
+console.log(num);
+console.log(msg);
+// msg = "Diego";
+// msg = 123;   no se puede realizar esto porque msg es un tipo string
---
+++ bases/app.ts
@@ -0,0 +1,8 @@
+const msg: string = 'Hola mundo';
+let num: number = 123;
+
+console.log(num);
+console.log(msg);
+
+// msg = "Diego";
+// msg = 123;   no se puede realizar esto porque msg es un tipo string
