45b9d7c8848204f537700bc128f8d2f0d1fe7b4c
Diego Pacori <dpacoria@unsa.edu.pe>
2023-04-18 18:32:54 -0500

    Se vio el modo observer tsc -watch or -w, para poder ver el cambio en ambos archivos y una mejor vision de los errores

--- bases/app.ts
+++ bases/app.ts
@@ -1,8 +1,14 @@
 const msg: string = 'Hola mundo';
 let num: number = 123;
+const heroe = {
+    name:'Ironman',
+    age: 45,
+}
+
+heroe.age = 50;
 
 console.log(num);
 console.log(msg);
-
+console.log(heroe.age + 1);
 // msg = "Diego";
 // msg = 123;   no se puede realizar esto porque msg es un tipo string
