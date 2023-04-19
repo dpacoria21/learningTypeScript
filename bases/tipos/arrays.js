a69286b5e359e999d678543eac63f83a02b5ea47
Diego Pacori <dpacoria@unsa.edu.pe>
2023-04-18 19:44:57 -0500

    Se trabajo con arrays en TS

---
+++ bases/tipos/arrays.js
@@ -0,0 +1,12 @@
+"use strict";
+(() => {
+    const numbers = [1, 2, 3, 4, 5, 6];
+    const villians = ['Omega Rojo', 'Dormamu', 'Duende Verde'];
+    villians.forEach(v => {
+        console.log(v.toUpperCase());
+    });
+    // const numbers: (string | number | boolean)[] = [1,2,3,4,5,'6',7,8,9,10];
+    // numbers.push(true);  
+    // numbers.push(11);
+    // console.log(numbers);
+})();
---
+++ bases/tipos/arrays.ts
@@ -0,0 +1,16 @@
+(() => {
+
+    const numbers: number[] = [1,2,3,4,5,6];
+    const villians: string[] = ['Omega Rojo', 'Dormamu', 'Duende Verde'];
+    villians.forEach( v => {
+        console.log(v.toUpperCase());
+    });
+
+
+    // const numbers: (string | number | boolean)[] = [1,2,3,4,5,'6',7,8,9,10];
+
+    // numbers.push(true);  
+    // numbers.push(11);
+
+    // console.log(numbers);
+})();
