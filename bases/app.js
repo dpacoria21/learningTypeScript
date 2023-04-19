c9fb00889c7490a5c52b76e43d6c8cb6a9d25006
Diego Pacori <dpacoria@unsa.edu.pe>
2023-04-18 20:34:39 -0500

    Se resolvio la tarea de TypeScript, aunque existen cosas que a dia de hoy en TypeScript no se acepta, pero en el examen aun no las toma

--- bases/app.js
+++ bases/app.js
@@ -1,5 +1,45 @@
 "use strict";
 (() => {
-    const a = 10;
-    console.log(a);
+    // Tipos
+    const batman = 'Bruce';
+    const superman = 'Clark';
+    const existe = false;
+    // Tuplas
+    const parejaHeroes = [batman, superman];
+    const villano = ['Lex Lutor', 5, true];
+    // Arreglos
+    const aliados = ['Mujer Maravilla', 'Acuaman', 'San', 'Flash'];
+    //Enumeraciones
+    let FuerzaLevel;
+    (function (FuerzaLevel) {
+        FuerzaLevel[FuerzaLevel["acuaman"] = 0] = "acuaman";
+        FuerzaLevel[FuerzaLevel["flash"] = 5] = "flash";
+        FuerzaLevel[FuerzaLevel["superman"] = 100] = "superman";
+        FuerzaLevel[FuerzaLevel["batman"] = 1] = "batman";
+    })(FuerzaLevel || (FuerzaLevel = {}));
+    const fuerzaFlash = FuerzaLevel.flash;
+    const fuerzaSuperman = FuerzaLevel.superman;
+    const fuerzaBatman = FuerzaLevel.batman;
+    const fuerzaAcuaman = FuerzaLevel.acuaman;
+    // Retorno de funciones
+    function activar_batiseñal() {
+        return 'activada';
+    }
+    function pedir_ayuda() {
+        console.log('Auxilio!!!');
+    }
+    const arr = [1, 2, 3, 4];
+    // Aserciones de Tipo
+    const poder = '100';
+    const largoDelPoder = poder.length;
+    // const largoDelPoder:number = (<string>poder).length;
+    console.log(largoDelPoder);
+    let enumeracion;
+    (function (enumeracion) {
+        enumeracion[enumeracion["a"] = 10] = "a";
+        enumeracion[enumeracion["b"] = 11] = "b";
+        enumeracion[enumeracion["c"] = 9] = "c";
+        enumeracion[enumeracion["d"] = 10] = "d";
+    })(enumeracion || (enumeracion = {}));
+    console.log(enumeracion.d);
 })();
--- bases/app.ts
+++ bases/app.ts
@@ -1,7 +1,61 @@
 (() => {
-    
-    const a: number = 10;
-    
-    console.log(a);
 
-})();
+    // Tipos
+    const batman: string = 'Bruce';
+    const superman: string = 'Clark';
+  
+    const existe: boolean = false;
+  
+    // Tuplas
+    const parejaHeroes: [string, string] = [batman,superman];
+    const villano: [string, number, boolean] = ['Lex Lutor',5,true];
+  
+    // Arreglos
+    const aliados: string[] = ['Mujer Maravilla','Acuaman','San', 'Flash'];
+  
+    //Enumeraciones
+
+    enum FuerzaLevel {
+      acuaman = 0,
+      flash = 5,
+      superman = 100,
+      batman = 1,
+    }
+
+    const fuerzaFlash: FuerzaLevel = FuerzaLevel.flash;
+    const fuerzaSuperman: FuerzaLevel  = FuerzaLevel.superman;
+    const fuerzaBatman: FuerzaLevel  = FuerzaLevel.batman;
+    const fuerzaAcuaman: FuerzaLevel  = FuerzaLevel.acuaman;
+  
+    // Retorno de funciones
+    function activar_batiseñal(): string{
+      return 'activada';
+    }
+  
+    function pedir_ayuda(): void{
+      console.log('Auxilio!!!');
+    }
+
+    const arr:any = [1,2,3,4];
+  
+    // Aserciones de Tipo
+    const poder: any = '100';
+    const largoDelPoder:number = (poder as string).length;
+    // const largoDelPoder:number = (<string>poder).length;
+    console.log( largoDelPoder );
+  
+    enum enumeracion {
+      a=10,
+      b,
+      c=9,
+      d
+    }
+
+    console.log(enumeracion.d);
+
+
+  })();
+
+
+  
+  
