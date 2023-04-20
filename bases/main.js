990d257e6b1fdba3f66e4b7376d5d6c8c1f48af0
Diego Pacori <dpacoria@unsa.edu.pe>
2023-04-20 14:23:31 -0500

    Se trabajo con la herencia y el encapsulamiento protected, el cual se encarga de trabajar como un privado pero como un publico frente a clases derivadas de este

--- bases/main.js
+++ bases/main.js
@@ -20,4 +20,29 @@
     console.log(antman.bio());
     console.log(Avenger.getAvgAge());
 })();
+(() => {
+    class Avenger {
+        constructor(name, realName) {
+            this.name = name;
+            this.realName = realName;
+            console.log('Constructor Avenger llamado!!!');
+        }
+        getFullName() {
+            return `${this.name} ${this.realName}`;
+        }
+    }
+    class Xmen extends Avenger {
+        constructor(name, realName, isMutant) {
+            super(name, realName);
+            this.isMutant = isMutant;
+            console.log('Constructor Xmen llamado!!!');
+        }
+        getFullNameXmen() {
+            console.log(super.getFullName());
+        }
+    }
+    const wolverine = new Xmen('Wolverine', 'Logan', true);
+    console.log(wolverine);
+    wolverine.getFullNameXmen();
+})();
 //# sourceMappingURL=main.js.map
