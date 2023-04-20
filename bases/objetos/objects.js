e37de6773e4a1ee128a58ada1bfe49e3ea0aae07
Diego Pacori <dpacoria@unsa.edu.pe>
2023-04-19 20:51:12 -0500

    Se trabajo con los type en TS, para crear una especie de modelo para nuestros objetos literales y que estos cumplan con ello

--- bases/objetos/objects.js
+++ bases/objetos/objects.js
@@ -5,13 +5,4 @@
         age: 24,
         powers: ['Super velocidad', 'Viajar en el tiempo'],
     };
-    flash = {
-        name: 'Clark Ken',
-        age: 60,
-        powers: ['Super fuerza'],
-        getNombre() {
-            return this.name;
-        }
-    };
-    console.log(flash.getNombre());
 })();
--- bases/objetos/objects.ts
+++ bases/objetos/objects.ts
@@ -1,20 +1,15 @@
 (() => {
 
+    type Hero = {
+        
+    }
+
     let flash: {name: string, age: number, powers: string[], getNombre?: () => string} = {
         name: 'Barry Alen',
         age: 24,
         powers: ['Super velocidad', 'Viajar en el tiempo'],
     };
 
-    flash = {
-        name: 'Clark Ken',
-        age: 60,
-        powers: ['Super fuerza'],
-        getNombre() {
-             return this.name;
-        }
-    }
 
-    console.log(flash.getNombre());
 
 })();
